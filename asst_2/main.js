30502a45a76b721d49d9fbf50ec4ac8940421cc9
oballiu <60414036+oballiu@users.noreply.github.com>
2020-04-01 14:39:38 -0400

    Update assignment 2 with the collaborative code

--- asst_2/main.js
+++ asst_2/main.js
@@ -1,13 +1,34 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", function(a) {
+    displayedImage.src = a.target.src;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+function darken_lighten() {
+  const BtnAttribute = btn.getAttribute("class");
+  if (BtnAttribute === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+}
+
+btn.addEventListener("click", function() {
+  darken_lighten();
+});
